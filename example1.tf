2057abc7dc0bc477f40d681253fae9af82b0c9fa
PJ Wilt <p.wilt.jr@gmail.com>
2020-01-01 21:13:15 -0500

    Add aws_key_pair

--- example1.tf
+++ example1.tf
@@ -3,7 +3,13 @@ provider "aws" {
 	region		= "us-east-2"
 }
 
+resource "aws_key_pair" "centos" {
+	key_name	= "centos"
+	public_key	= "${file("centos.pub")}"
+}
 resource "aws_instance" "example" {
 	ami				= "ami-d9e3c4bc"
 	instance_type	= "t2.micro"
+	key_name		= "centos"
+	user_data		= "${file("userdata.sh")}"
 }

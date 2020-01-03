72fb5ec9131a2b84b6d52e3427161614912cffcd
PJ Wilt <p.wilt.jr@gmail.com>
2020-01-02 20:35:23 -0500

    Ran terraform fmt

--- example1.tf
+++ example1.tf
@@ -1,30 +1,30 @@
 provider "aws" {
-	profile 	= "default"
-	region		= "us-east-2"
+  profile = "default"
+  region  = "us-east-2"
 }
 
 resource "tls_private_key" "centos-test-key" {
-	algorithm	= "RSA"
-	rsa_bits	= 4096
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
 
 resource "aws_key_pair" "centos" {
-	key_name	= "centos-test-key"
-	public_key	= tls_private_key.centos-test-key.public_key_openssh
+  key_name   = "centos-test-key"
+  public_key = tls_private_key.centos-test-key.public_key_openssh
 }
 
 resource "aws_instance" "centos-test" {
-	ami				= "ami-0f2b4fc905b0bd1f1"
-	instance_type	= "t2.micro"
-	key_name		= aws_key_pair.centos.key_name
-	user_data		= file("userdata.sh")
+  ami           = "ami-0f2b4fc905b0bd1f1"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.centos.key_name
+  user_data     = file("userdata.sh")
 }
 
 resource "local_file" "centos-test-key-pem" {
-	content		= tls_private_key.centos-test-key.private_key_pem
-	filename	= "${aws_key_pair.centos.key_name}.pem"
+  content  = tls_private_key.centos-test-key.private_key_pem
+  filename = "${aws_key_pair.centos.key_name}.pem"
 }
 
 output "public-dns" {
-	value = aws_instance.centos-test.public_dns
+  value = aws_instance.centos-test.public_dns
 }

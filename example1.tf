4c9933c0941d7f2432a4fa473efc0aeccfc01af5
PJ Wilt <p.wilt.jr@gmail.com>
2020-01-02 19:52:27 -0500

    Changed to tf 0.12 syntax, tf now generates the key in the key_pair

--- example1.tf
+++ example1.tf
@@ -3,9 +3,28 @@ provider "aws" {
 	region		= "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "tls_private_key" "centos-test-key" {
+	algorithm	= "RSA"
+	rsa_bits	= 4096
+}
+
+resource "aws_key_pair" "centos" {
+	key_name	= "centos-test-key"
+	public_key	= tls_private_key.centos-test-key.public_key_openssh
+}
+
+resource "aws_instance" "centos-test" {
 	ami				= "ami-0f2b4fc905b0bd1f1"
 	instance_type	= "t2.micro"
-	key_name		= "CentOS7"
-	user_data		= "${file("userdata.sh")}"
+	key_name		= aws_key_pair.centos.key_name
+	user_data		= file("userdata.sh")
+}
+
+resource "local_file" "centos-test-key-pem" {
+	content		= tls_private_key.centos-test-key.private_key_pem
+	filename	= "${aws_key_pair.centos.key_name}.pem"
+}
+
+output "public-dns" {
+	value = aws_instance.centos-test.public_dns
 }
